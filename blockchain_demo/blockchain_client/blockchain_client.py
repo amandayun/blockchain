5ababa274f33de88a8ba9e77c0ce19fb9cdedee0
Amanda Yun <akyun@ucsc.edu>
2022-01-23 23:09:04 -0800

    updating blockchain project files

--- blockchain_demo/blockchain_client/blockchain_client.py
+++ blockchain_demo/blockchain_client/blockchain_client.py
@@ -21,3 +21,21 @@ def make_transaction():
 @app.route('/view/transactions')
 def view_transactions():
 	return render_template('view_transactions.html')
+
+#allow user to generate a new wallet
+
+@app.route('/wallet/new')
+def new_wallet():
+	return ''
+
+
+if __name__ == '__main__':
+	from argparse import ArgumentParser
+	
+	parser = ArgumentParser()
+	parser.add_argument('-p', '--port', default=8081, type=int, help="port to listen to")
+	args = parser.parse_args()
+	port = args.port
+	
+	#host = local host
+	app.run(host='127.0.0.1', port=port, debug=True)
